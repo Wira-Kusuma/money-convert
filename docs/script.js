fe6f9dcc3d55e11d1a76a244b27fd3219d6f9f34
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-05-24 20:50:31 +0700

    add multiple currency

--- docs/script.js
+++ docs/script.js
@@ -4,10 +4,32 @@ function konversi() {
 
     let hasil = parseFloat(dolar) * rupiah;
 
-document.getElementById("output").innerText =
-  "Hasil konversi: " + hasil.toLocaleString("id-ID", {
+    document.getElementById("output").innerText =
+    "Hasil konversi: " + hasil.toLocaleString("id-ID", {
     style: "currency",
     currency: "IDR"
-  });
+    });
 
+}
+function konversi2() {
+    let rupiah = document.getElementById("rupiah").value
+    let dolar = 1;
+
+    let hasil = parseFloat(dolar) * rupiah;
+
+    document.getElementById("keluaran").innerText =
+    "Hasil konversi: " + hasil.toLocaleString("en-EN", {
+    // style: "currency",
+    currency: "USD"
+    });
+
+}
+
+function dr() {
+    document.getElementById("cont2").style.display="none";
+    document.getElementById("cont").style.display="flex";
+}
+function rd() {
+    document.getElementById("cont2").style.display="none";
+    document.getElementById("contrd").style.display="flex";
 }
