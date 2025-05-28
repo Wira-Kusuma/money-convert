6fd4bb34b1540598e4a415522f15a2b7b25a6e2f
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-05-28 21:47:25 +0700

    setTimeout

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function konversi() {
-    let dolar = document.getElementById("dolar").value
-    let rupiah = 16246;
+  let dolar = document.getElementById("dolar").value;
+  let rupiah = 16246;
+  let hasil = parseFloat(dolar) * rupiah;
 
-    let hasil = parseFloat(dolar) * rupiah;
-
-    document.getElementById("output").innerText =
-    "Hasil konversi: " + hasil.toLocaleString("id-ID", {
-    style: "currency",
-    currency: "IDR"
-    });
-
+  
+    document.getElementById("output").innerText ="Hasil konversi: " + hasil.toLocaleString("id-ID", {
+        style: "currency",
+        currency: "IDR"
+      });
 }
+
 function konversi2() {
   let rupiah = document.getElementById("rupiah").value;
   let dolar = 16246; // 1 USD = 16.246 IDR (kurs)
@@ -26,10 +25,14 @@ function konversi2() {
 
 
 function dr() {
-    document.getElementById("cont2").style.display="none";
-    document.getElementById("cont").style.display="flex";
+    setTimeout(() =>{
+      document.getElementById("cont2").style.display="none";
+      document.getElementById("cont").style.display="flex";
+    },400)
 }
 function rd() {
-    document.getElementById("cont2").style.display="none";
-    document.getElementById("contrd").style.display="flex";
+    setTimeout(() => {
+      document.getElementById("cont2").style.display="none";
+      document.getElementById("contrd").style.display="flex";
+    },400)
 }
